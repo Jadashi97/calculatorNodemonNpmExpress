fd21082f3b93027ad9075d6d22b7ab69e3a2a352
jadashi97 <njadauniversal23@yahoo.com>
2022-04-23 09:23:54 -0500

    (feat): build a calc, incoorporate nodemon, js, express and html to get inputs

--- calculator.js
+++ calculator.js
@@ -3,7 +3,15 @@
 // we initialise express to set it up
 const express = require("express");
 
+//this incoorporates body parser into our code
+var bodyParser = require('body-parser');
+
 const app = express();
+
+//this is how we use to body parser to grab inputs from the user into our servers 
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 // this is used to send the whole file and (__dirname) is just the path where the html file exists
 
@@ -14,13 +22,23 @@ app.get("/", function(req, res){
 //this is to used to post something onto the browser and allows user to see content when request happens
 
 app.post("/", function(req, res){
-    res.send(" keeping moving nothind to seee here!!!");
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send(`The result of the calculation is ${result}`);
 });
 
-// just me goofing around and testing if this works
-app.get("/wazzaa", function(req, res){
-    res.send("yoooooo. look at these booooomers")
-});
+
+
+// // just me goofing around and testing if this works
+// app.get("/wazzaa", function(req, res){
+//     res.send("yoooooo. look at these booooomers")
+// });
+
+// this listens to that port to show any that our server is running on that port 
 
 app.listen(3000, function(){
     console.log("my calculator server is up and  running on port 3000!!");
